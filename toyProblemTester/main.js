041a95712b2ff6e974b82c739c7741d4bba83adb
Marc Christophe <m@xtof.net>
2016-05-25 11:49:11 -0700

    reporter now outputs to file

--- toyProblemTester/main.js
+++ toyProblemTester/main.js
@@ -11,7 +11,6 @@ var github = new Github({
   auth: 'basic'
 });
 
-
 var orgRepo = github.getRepo(secrets.orgName, secrets.repo);
 var mkdir = Q.nfbind(fs.mkdir);
 var unlink = Q.nfbind(fs.unlink);
@@ -19,7 +18,7 @@ var rmdir = Q.nfbind(fs.rmdir);
 
 var writeFile = function(data, filename) {
   var deferred = Q.defer();
-  console.log('write file: ', filename);
+  //console.log('write file: ', filename);
   fs.writeFile('./temp/' + filename, data, 'utf8', function(err) {
     console.log(err);
     deferred.resolve();
@@ -29,27 +28,23 @@ var writeFile = function(data, filename) {
 
 var getTestFile = function(problemName) {
   var deferred = Q.defer();
-  console.log('get test file');
+  //console.log('get test file');
   orgRepo.read('solution', problemName + '/' + problemName + '.test.js', function(err, data) {
     if (err) {
       console.log(err);
     }
     deferred.resolve(data);
   });
-
   return deferred.promise;
 };
 
 var getStudentFile = function(studentUser, problemName) {
   var deferred = Q.defer();
   var studentRepo = github.getRepo(studentUser, secrets.repo);
-
   studentRepo.read('master', problemName + '/' + problemName + '.js', function(err, data) {
-
     if (err) {
       console.log(err);
     }
-
     deferred.resolve(data);
   });
   return deferred.promise;
@@ -70,47 +65,44 @@ var checkSyntax = function(data) {
 };
 
 var runTests = function() {
-
   var deferred = Q.defer();
-
   var mocha = new Mocha({
+    //reporter: 'list' 
     reporter: 'custom-reporter'
   });
-
   mocha.addFile('./temp/subject.js');
-  mocha.addFile('./temp/test.js');
-
+  mocha.addFile('./temp/subject.test.js');
   var log = console.log;
-  //console.log = function() {};
+  console.log = function() {};
   log('running tests...');
-
+  process.env.CUSTOM_REPORTER_TARGET_FILE = 'somecrap.txt';
   mocha.run(function(failures) {
-    log('ar gsI', arguments);
-    process.on('exit', function() {
-      log(arguments);
-      //process.exit(failures);
-      console.log = log;
-      deferred.resolve();
-    });
+    log('argsI', arguments);
+    console.log = log;
+    deferred.resolve();
+    // process.on('exit', function() {
+    //   log('food', arguments); 
+    //   //process.exit(failures);
+    //   deferred.resolve(failures);
+    // });
   }, function() {
     log(arguments);
     console.log = log;
     deferred.reject('something happened');
   });
-
   return deferred.promise;
 };
 
-var toyProblemName = 'primeTester';
+var toyProblemName = 'rotatedArraySearch';
 mkdir('./temp/')
   .then(function() {
     return getTestFile(toyProblemName);
   })
   .then(function(data) {
-    return writeFile(data, 'test.js');
+    return writeFile(data, 'subject.test.js');
   })
   .then(function() {
-    return getStudentFile(secrets.studentUsernames[1], toyProblemName);
+    return getStudentFile(secrets.studentUsernames[6], toyProblemName);
   })
   .then(function(data) {
     return checkSyntax(data);
@@ -122,30 +114,31 @@ mkdir('./temp/')
     return runTests();
   })
   .then(function() {
+    //jconsole.log('eat shit');
     return unlink('./temp/subject.js');
   })
   .then(function() {
-    return unlink('./temp/test.js');
+    return unlink('./temp/subject.test.js');
   })
   .then(function() {
     return rmdir('./temp/');
   })
   .then(function() {
-    console.log(arguments);
+    //console.log(arguments);
     console.log('finis');
   })
   .catch(function(err) {
-    console.log('ERROR$$:  '+ err);
+    console.log('ERROR$$:  ' + err);
     switch (err.message) {
       case 'SYNTAX ERROR':
-        unlink('./temp/test.js')
+        unlink('./temp/subject.test.js')
           .then(function() {
             return rmdir('./temp/');
           })
           .done();
         break;
       default:
-        unlink('./temp/test.js')
+        unlink('./temp/subject.test.js')
           .then(function() {
             return unlink('./temp/subject.js');
           })
--- toyProblemTester/my-modules/custom-reporter/custom-reporter.js
+++ toyProblemTester/my-modules/custom-reporter/custom-reporter.js
@@ -1,24 +1,37 @@
+var fs = require('fs');
+
 module.exports = CustomReporter;
 
 function CustomReporter(runner) {
   var passes = 0;
   var failures = 0;
+  var txt = '';
 
   runner.on('pass', function(test) {
     passes++;
     process.stdout.write('pass: ' + test.fullTitle());
     process.stdout.write('\n  ');
+    txt += 'pass: ' + test.fullTitle();
+    txt += '\n';
   });
 
   runner.on('fail', function(test, err) {
     failures++;
     process.stdout.write('fail: ' + test.fullTitle() + ' -- error: ' + err.message);
     process.stdout.write('\n  ');
+
+    txt += 'fail: ' + test.fullTitle() + ' -- error: ' + err.message;
+    txt += '\n';
   });
 
   runner.on('end', function() {
     process.stdout.write('end: ' + passes + '/' + (passes + failures));
     process.stdout.write('\n  ');
-    process.exit(failures);
+    //process.exit(failures);
+
+    txt += 'end: ' + passes + '/' + (passes + failures);
+    txt += '\n';
+
+    fs.writeFileSync(process.env.CUSTOM_REPORTER_TARGET_FILE, txt);
   });
 }
